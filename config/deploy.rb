c6bd9908291a4e45f79cd201e210bc156a2eb4cd
Daniel Braz <dsbraz@gmail.com>
2012-02-12 01:06:55 -0200

    Adicionando suporte ao apache ao cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,13 @@
 $:.unshift(File.expand_path('./lib', ENV['rvm_path']))
 require "rvm/capistrano"
 
-set :rvm_type, :system
-
-set :application, "curvatub-orders"
-set :scm, :git
+set :rvm_type,    :system
+set :scm,         :git
 set :repository,  "git@github.com:dsbraz/curvatub-orders.git"
-set :deploy_to, "/opt/curvatub/webapps/orders"
-set :use_sudo, false
-set :user, "root"
+set :application, "curvatub-orders"
+set :deploy_to,   "/opt/curvatub/webapps/orders"
+set :user,        "root"
+set :use_sudo,    false
 
 server "orders.curvatub.com.br", :app, :web, :db, :primary => true
 
@@ -17,5 +16,13 @@ namespace :deploy do
   task :stop do ; end
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
+namespace :apache do
+  [:stop, :start, :restart, :reload].each do |action|
+    task action, :roles => :web do
+      invoke_command "/etc/init.d/apache2 #{action.to_s}", :via => run_method
+    end
   end
 end
